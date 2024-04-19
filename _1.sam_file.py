db3d0eae1b686dcb9fe3c7ad398b05778112b278
nithin88844 <122030484+nithin88844@users.noreply.github.com>
2024-04-19 14:34:50 +0530

    third commit

--- _1.sam_file.py
+++ _1.sam_file.py
@@ -3,3 +3,10 @@ print("Hello   World")
 def loop(n):
     for i in range(n):
         print(i)
+
+def prime(x):
+    for i in range(x):
+        if x%i==0:
+            print(f"{x} is not a prime number")
+    else:
+        print(f"{x} is a prime number")
