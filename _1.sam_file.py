606321f22ddb5347b36c8a746f248a189a3481ea
nithin88844 <122030484+nithin88844@users.noreply.github.com>
2024-10-31 13:58:11 +0530

    first commit

--- _1.sam_file.py
+++ _1.sam_file.py
@@ -1,12 +1,5 @@
-print("Hello   World")
+import streamlit as st
 
-def loop(n):
-    for i in range(n):
-        print(i)
-
-def prime(x):
-    for i in range(x):
-        if x%i==0:
-            print(f"{x} is not a prime number")
-    else:
-        print(f"{x} is a prime number")
+st.title("Hai Nithin Satheesh")
+st.write("Hope You are Doing Well!")
+st.write("How are you?")
